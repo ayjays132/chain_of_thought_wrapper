c7b8e7a0f87efd92632a3a77f4ad561b97368b8b
ayjays132 <Ayjays.ph@gmail.com>
2025-07-22 17:29:47 -0700

    Update README and fix __init__ circular import

--- cot_toolkit/__init__.py
+++ cot_toolkit/__init__.py
@@ -1,12 +1,21 @@
 from importlib.metadata import version
 
-from chain_of_thought_wrapper import (
-    ChainOfThoughtWrapper,
-    validate_device_selection,
-    normalize_answer,
-)
 from .benchmark import benchmark_prompt
 from .simple_rag import SimpleRAG
+
+# Functions are imported lazily to avoid circular imports when
+# ``chain_of_thought_wrapper`` itself depends on this package.
+def ChainOfThoughtWrapper(*args, **kwargs):
+    from chain_of_thought_wrapper import ChainOfThoughtWrapper as _COT
+    return _COT(*args, **kwargs)
+
+def validate_device_selection(*args, **kwargs):
+    from chain_of_thought_wrapper import validate_device_selection as _validate
+    return _validate(*args, **kwargs)
+
+def normalize_answer(*args, **kwargs):
+    from chain_of_thought_wrapper import normalize_answer as _normalize
+    return _normalize(*args, **kwargs)
 
 __all__ = [
     "ChainOfThoughtWrapper",
